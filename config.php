11a7eb1f4bcf9dd2a2000cc1496f11620f725f3c
Pedro Henrique <prdrinho13@gmail.com>
2024-02-23 15:34:26 -0300

    fix: :bug: .

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $dbUsername = 'pedro';
 $dbPassword = '2007';
 $dbName = 'formulario_pedro';
 
-$conexaoM = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+$conexaoM = new mysqli($dbHost, $dbUsername,$dbPassword, $dbName);
 
 //Conexão com o banco de dados é importante não alterar pois é referente as informações colocada no MySQL.
 //connection to the database is important not to change as it refers to the information placed in MySQL.
