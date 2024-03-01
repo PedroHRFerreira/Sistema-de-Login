cb08e8dd56245fec1f7ba5877c2e11977c7265e9
Pedro Henrique <prdrinho13@gmail.com>
2024-03-01 15:48:51 -0300

    fix: :bug: mudação de title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Cadastro</title>
+    <title>Cadastro ;)</title>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     <link rel="stylesheet" type="text/css" href="css/mediaIndex.css" />
   </head>
